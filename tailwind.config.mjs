4b13dd8d43e1464f1ba664a890d135031e329d49
Lucemans <luc@lucemans.nl>
2024-05-31 20:28:05 +0300

    Update Title, and Theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: ['./src/**/*.{mjs,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+    content: ["./src/**/*.{mjs,js,ts,jsx,tsx}"],
+    theme: {
+        extend: {}
+    },
+    plugins: []
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-    setupFiles: '.vitest/setup',
-    include: ['**/test.{ts,tsx}']
-  }
-})
+    plugins: [react(), tsconfigPaths()],
+    test: {
+        globals: true,
+        environment: "happy-dom",
+        setupFiles: ".vitest/setup",
+        include: ["**/test.{ts,tsx}"]
+    }
+});
