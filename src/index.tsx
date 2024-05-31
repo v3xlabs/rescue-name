4bd0d8a07c1d92eb1e54c06f344c0ce2d9ba39fa
Lucemans <luc@lucemans.nl>
2024-05-31 20:18:45 +0300

    Initialize ESLint

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
-import { createRoot } from 'react-dom/client'
-import 'tailwindcss/tailwind.css'
-import App from 'components/App'
+import 'tailwindcss/tailwind.css';
 
-const container = document.getElementById('root') as HTMLDivElement
-const root = createRoot(container)
+import App from 'components/App';
+import { createRoot } from "react-dom/client";
 
-root.render(<App />)
+const container = document.querySelector('#root') as HTMLDivElement;
+const root = createRoot(container);
+
+root.render(<App />);
