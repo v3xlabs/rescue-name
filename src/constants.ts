92302dab5ed37802bb60824933654b68e7b41ff8
Lucemans <luc@lucemans.nl>
2024-06-01 22:01:28 +0000

    yes this is a commit

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,5 @@ import { mainnet, sepolia } from "wagmi/chains";
 
 export const CONTRACT_ADDRESS: { [key: number]: Address } = {
     [mainnet.id]: "0x0",
-    [sepolia.id]: "0xf8078fdDF72FEc08bEB3d128CaBBD8525837a265"
+    [sepolia.id]: "0x76df9537df71a0847625add11e73d780adb74a71"
 };
