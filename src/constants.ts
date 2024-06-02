9bdbc3eff4d28741e0ab07c90066bc6e9ffdddc1
Lucemans <luc@lucemans.nl>
2024-06-02 00:59:57 +0000

    Update deployment

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,5 @@ import { mainnet, sepolia } from "wagmi/chains";
 
 export const CONTRACT_ADDRESS: { [key: number]: Address } = {
     [mainnet.id]: "0x0",
-    [sepolia.id]: "0x78AC7f4856aD34A170CE1B8dD9f9996FC6E5a291"
+    [sepolia.id]: "0x8478988df937aeE1b9b25f6016ee5A45eBc1bb39"
 };
