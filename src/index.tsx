1f66e3e69a9ebf63a4c9e6d55b4dfbe23da6080a
Lucemans <luc@lucemans.nl>
2024-06-02 03:55:36 +0000

    Introduce HTTP Redirect

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,11 @@ import { WagmiProvider } from "wagmi";
 import { App } from "./components/App";
 import { wagmiConfig } from "./wagmi.config";
 
+// Redirect http to https
+if (location.protocol === "http:" && location.hostname !== "localhost") {
+    location.replace("https:" + location.href.slice(location.protocol.length));
+}
+
 const container = document.querySelector("#root") as HTMLDivElement;
 const root = createRoot(container);
 
