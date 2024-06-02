da44906415ceddbcbff764432e582402dcf7a231
JustAnotherDevv <daniel_zarzecki@wp.pl>
2024-06-02 05:47:09 +0200

    added graphql and names page

--- src/components/names/NameEntry.tsx
+++ src/components/names/NameEntry.tsx
@@ -1,26 +1,50 @@
+import clsx from "clsx";
 import { useExpiry } from "hooks/useExpiry";
-import { FC, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-export const NameEntry: FC<{
+import { formatDate } from "../../utils";
+
+interface NameEntryProperties {
     name: string;
-    onExpiryUpdate: (name: string, expiry: bigint) => void;
-}> = ({ name, onExpiryUpdate }) => {
-    const { data: expiry, error } = useExpiry(name);
+}
+
+export const NameEntry: React.FC<NameEntryProperties> = (properties) => {
+    const [selected, setSelected] = useState(false);
+    const { data: expiry, error } = useExpiry(properties.name);
 
     useEffect(() => {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         if (expiry?.expiry.value) {
-            onExpiryUpdate(name, expiry!.expiry.value);
+            properties.onExpiryUpdate(name, expiry!.expiry.value);
         }
-    }, [expiry, name, onExpiryUpdate]);
+    }, [expiry, name, properties.onExpiryUpdate]);
+
+    const handleClick = () => {
+        setSelected(!selected);
+        properties.onSelect();
+    };
 
     return (
-        <div className="flex w-full items-center justify-between">
-            <div>{name}</div>
-            <div>
-                {expiry && <div>{JSON.stringify(expiry.expiry.date)}</div>}
-                {error && <div>{error.message}</div>}
+        <button
+            className={clsx(
+                "card my-2 flex w-full flex-col gap-2 p-4 pt-0",
+                selected && "bg-background-disabled"
+            )}
+            onClick={handleClick}
+        >
+            <div className="flex w-full flex-col">
+                <div className="flex items-center justify-between">
+                    <div className="flex flex-col items-start">
+                        <div className="font-semibold">{properties.name}</div>
+                        <div className="text-xs font-medium">Vault: 0</div>
+                    </div>
+                    <div>
+                        {expiry
+                            ? formatDate(expiry.expiry.date.toString())
+                            : ""}
+                    </div>
+                </div>
             </div>
-        </div>
+        </button>
     );
 };
